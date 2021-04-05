cef8a2e56ab4f10f9cdec6031dd82bfa74d2d6af
MasonWang025 <mason.wang0025@gmail.com>
2021-04-05 16:40:05 -0700

    tailwind + postcss config setup, typescript setup, added Page.tsx utility component

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Page from "../components/utility/Page";
+
+export default function Home() {
+  return (
+    <Page
+      title="TogaHacks I"
+      desc="Exploring economics. Empowering leaders. Official page for the Youth Economics Intitiative (YEI)."
+    >
+      <h1>test</h1>
+      <p>test</p>
+    </Page>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    ,
+    "./data/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  corePlugins: {
+    ringWidth: false,
+    outline: false,
+  },
+  theme: {
+    extend: {
+      colors: {
+       
+      },
+    },
+  },
+  variants: {
+    extend: {
+      cursor: ["disabled"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
